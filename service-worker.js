c24b95fa528baca451172dd9b87e3e3ba86dbb35
mfeltenmark <mikael@techchange.io>
2025-11-25 10:36:50 +0100

    update of service worker
    update of service worker

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,31 @@
-self.addEventListener("install", (event) => {
-  // Force the service worker to become active immediately
-  self.skipWaiting();
+const CACHE_NAME = "counter-cache-v1";
+const urlsToCache = [
+  "/counter/",
+  "/counter/index.html",
+  "/counter/manifest.json",
+  "/counter/icons/icon-192x192.png",
+  "/counter/icons/icon-512x512.png"
+];
+
+// Installera service workern och cacha alla filer
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener("activate", (event) => {
-  // Take control of all pages immediately
-  clients.claim();
+// Aktivera service workern direkt
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+// Hantera fetch: returnera cache om offline
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
